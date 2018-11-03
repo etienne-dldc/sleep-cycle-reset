b90a61514208839fd087d157f4ef11907fede0a4
Etienne Dldc <e.deladonchamps@gmail.com>
2018-11-03 10:33:27 +0100

    Fix

--- src/logic/state.ts
+++ src/logic/state.ts
@@ -42,6 +42,7 @@ export type State = {
   config: Config;
   endWeekday: Derive<Weekday>;
   dayLength: Derive<number>;
+  numberOfNights: Derive<number>;
   wakeUpDiff: Derive<number>;
   planning: Derive<Array<{ up: PlanningItem; down: PlanningItem }>>;
   totalHours: Derive<number>;
@@ -63,15 +64,15 @@ const state: State = {
   wakeUpDiff: state => {
     return state.config.endHour + 24 - state.config.startHour;
   },
+  numberOfNights: state => state.config.days - 1,
   totalHours: state => 24 - state.config.startHour + (state.config.days - 1) * 24 + state.config.endHour,
-  dayLength: state => state.totalHours / (state.config.days - 1),
+  dayLength: state => state.totalHours / state.numberOfNights,
   planning: state => {
     const startDayIndex = allWeekdays.indexOf(state.config.startWeekday);
-    const numOfNights = state.config.days - 1;
-    const extendedDayLength = Math.floor(state.totalHours / numOfNights);
-    const longerDays = state.totalHours - extendedDayLength * numOfNights;
+    const extendedDayLength = Math.floor(state.totalHours / state.numberOfNights);
+    const longerDays = state.totalHours - extendedDayLength * state.numberOfNights;
     let acc = state.config.startHour;
-    return new Array(numOfNights).fill(null).map((_, index) => {
+    return new Array(state.numberOfNights).fill(null).map((_, index) => {
       const dayLength = extendedDayLength + (index < longerDays ? 1 : 0);
       acc += dayLength;
       const upHour = acc;
